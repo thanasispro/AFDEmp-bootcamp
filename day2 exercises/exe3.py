62e6595a4c96b9544966371e33a1f1b1bf26be9a
thanasispro <protopapas.dim.ath@outlook.com>
2016-09-27 20:27:50 +0300

    Update exe3.py

--- day2 exercises/exe3.py
+++ day2 exercises/exe3.py
@@ -1,7 +1,10 @@
 import math
-a= int(input("The side a is:"))
-b= int(input("The side b is:"))
-c= int(input("The side c is:"))
-e=(a+b+c)*(-a+b+c)*(a-b+c)*(a+b-c)
-k=0.25*math.sqrt(e)
-print("The area is",k)
+a= float(input("The side a is:"))
+b= float(input("The side b is:"))
+c= float(input("The side c is:"))
+e=(a+b+c)*(b-a+c)*(a-b+c)*(a+b-c)
+if e>=0:
+    k=0.25*math.sqrt(e)
+    print("The area is",k)
+else:
+    print("this sides can't make a triangle, please check again!")
