8d1472b4ca250e02d3377682c74679edaec312f4
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2016-09-30 03:12:10 +0300

    Update exe1d3.py

--- day3 exercises/exe1d3.py
+++ day3 exercises/exe1d3.py
@@ -1,17 +1,17 @@
 idn=input('PLEASE ENTER YOUR TAX INDENTIFICATION NUMBER:')
-number=[int(x) for x in idn[0:9]]
+number=[int(x) for x in idn[0:9]] #ftiaxnoume pinaka
 sum=0
-new=number[:8]
-new.reverse()
+new=number[:8]    #afairoum to teleutaio psifio
+new.reverse()     #reverse
 if len(number) != 9:
-    print('Please enter a valid number with 9 digits')
+    print('Please enter a valid number with 9 digits')  #proairetiko
     
 else:        
  sum=0
  n=0
  for i in range(1,8):
   n=new[i-1]* 2**i
-  sum=sum+n
+  sum=sum+n       
 
 p=sum%11
 last=p%10
