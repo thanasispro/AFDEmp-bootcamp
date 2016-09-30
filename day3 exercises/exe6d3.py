f00ba608f9db3d8c7f1c81b8f79550dbf0ab83ea
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2016-09-30 03:23:41 +0300

    Update exe6d3.py

--- day3 exercises/exe6d3.py
+++ day3 exercises/exe6d3.py
@@ -3,14 +3,14 @@ two=[]
 three=[]
 alln=[]
 for i in range(3):
-   num1=int(input('ENTER A NUMBER WITH 1DIGIT:'))
+   num1=int(input('ENTER A NUMBER WITH 1DIGIT:'))   #loop gia tin eisodo arithmwn kai dimiourgia enos megalou pinaka
    alln.append(num1)
    num2=int(input('ENTER A NUMBER WITH 2DIGIT:'))
    alln.append(num2)         
    num3=int(input('ENTER A NUMBER WITH 3DIGIT:'))
    alln.append(num3)
 
-for i in range(9):
+for i in range(9):           #dimiourgia pinakwn analoga me twn aritmo twn psifiwn
     if len(str(alln[i]))==1:
         one.append(alln[i])
     elif len(str(alln[i]))==2:
@@ -18,10 +18,10 @@ for i in range(9):
     else:
         three.append(alln[i])
         
-print("Enter numbers:",*alln)
+print("Enter numbers:",*alln)    #ektipwsi megalou pinaka
 
     
-for i in range(3):
+for i in range(3):     #ektupwsi twn triwn pinakwn me tim morfi pou zhththike
    print(" ",one[i],end="|")
 print()
 for i in range(3):
