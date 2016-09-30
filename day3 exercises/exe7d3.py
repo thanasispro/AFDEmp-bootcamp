2338c70eb11dba5037100c73d1f184cc57d69be6
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2016-09-30 20:54:03 +0300

    Update exe7d3.py

--- day3 exercises/exe7d3.py
+++ day3 exercises/exe7d3.py
@@ -1,19 +1,18 @@
-date1=input('Enter a date(mm/dd/yyy)') #eisagwgh stin  morfi imerominias
-d1=date1.split("/") #diaxwrismos kai paragwgh pinakwn 
+date1=input('Enter a date(mm/dd/yyyy)')
+d1=date1.split("/")
 d1=[int(i) for i in d1]
-date2=input('Enter a date(mm/dd/yyy)')
+date2=input('Enter a date(mm/dd/yyyy)')
 d2=date2.split("/")
 d2=[int(i) for i in d2]
+print(d1)
+print(d2)
 
-c1=365*abs(d1[2])+((abs(d1[2]))/4)-((abs(d1[2]))/100)+((abs(d1[2]))/400)+((306*d1[1]+5)/10) +(d1[0]-1)
+c1=365*abs(d1[2])+(abs(d1[2])/4)-(abs(d1[2])/100)+(abs(d1[2])/400)+((306*d1[0]+5)/10) +(d1[1]-1)
 
-c2=365*abs(d2[2])+((abs(d2[2]))/4)-((abs(d2[2]))/100)+((abs(d2[2]))/400)+((306*d2[1]+5)/10)+(d2[0]-1)
+c2=365*abs(d2[2])+(abs(d2[2])/4)-(abs(d2[2])/100)+(abs(d2[2])/400)+((306*d2[0]+5)/10)+(d2[1]-1)
 
-#mathimatikoi tupoi
-if c1>c2:
-   diafora=c1-c2
-else:
-    diafora=c2-c1
-print("Enter dates:",*d1,sep='/',end=' ') #elegxos megaluterwn kai ektupwsh
+
+
+print("Enter dates:",*d1,sep='/',end=' ')
 print(*d2,sep='/')
-print(int(diafora),"days")
+print(int(abs(c2-c1)),"days")
