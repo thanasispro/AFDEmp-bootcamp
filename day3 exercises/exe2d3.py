dc065040cf4fb9b69fbb6b2f4b3952316cea82e2
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2016-09-30 03:14:23 +0300

    Update exe2d3.py

--- day3 exercises/exe2d3.py
+++ day3 exercises/exe2d3.py
@@ -2,11 +2,11 @@ num=input('PLEASE ENTER THE BINARY NUMBER:')
 number=[int(x) for x in num[0:8]]
 parity=number[0:7]
 if len(num) != 8:
-    print('Ooooops 8bit number:')
+    print('Ooooops 8bit number:') #proairetiko
 else:
     sum1=0
     sum2=0
-    for i in  range(0,7):
+    for i in  range(0,7): # ta athroismata gia tous 8digit kai 7digit
         if parity[i]==1:
             sum1=sum1+1
     for i in range(0,8):
@@ -14,7 +14,7 @@ else:
             sum2=sum2+1
 
 
-if sum1%2==0:
+if sum1%2==0:     #monos i zugos arithmos kai elegxos
     if sum1==sum2:
         print("Parity check OK")
     else:
