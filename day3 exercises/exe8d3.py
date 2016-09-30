173f4cbb6020070de0d9ad97baa6185415c92a74
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2016-09-30 03:28:23 +0300

    Update exe8d3.py

--- day3 exercises/exe8d3.py
+++ day3 exercises/exe8d3.py
@@ -1,9 +1,9 @@
-num=input("ENETER A SEQUENCE OF NUMBERS:")
+num=input("ENETER A SEQUENCE OF NUMBERS:") #pinakas twn psifiwn
 number=[int(x) for x in num[0:]]
 k=len(number)
 sum=0
 
-if k%2!=0:
+if k%2!=0:    #elegxos an einai monos i zugos arithmos kai oi prakseis
     for i in range(1,k,2):
         sum=sum+number[i-1]*number[i]
     teliko=sum+number[k-1]
