3c03c46e6cadbf84cb77dd7d8f02421c76fef03c
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2016-09-30 03:26:43 +0300

    Update exe7d3.py

--- day3 exercises/exe7d3.py
+++ day3 exercises/exe7d3.py
@@ -1,5 +1,5 @@
-date1=input('Enter a date(mm/dd/yyy)')
-d1=date1.split("/")
+date1=input('Enter a date(mm/dd/yyy)') #eisagwgh stin  morfi imerominias
+d1=date1.split("/") #diaxwrismos kai paragwgh pinakwn 
 d1=[int(i) for i in d1]
 date2=input('Enter a date(mm/dd/yyy)')
 d2=date2.split("/")
@@ -9,11 +9,11 @@ c1=365*abs(d1[2])+((abs(d1[2]))/4)-((abs(d1[2]))/100)+((abs(d1[2]))/400)+((306*d
 
 c2=365*abs(d2[2])+((abs(d2[2]))/4)-((abs(d2[2]))/100)+((abs(d2[2]))/400)+((306*d2[1]+5)/10)+(d2[0]-1)
 
-
+#mathimatikoi tupoi
 if c1>c2:
    diafora=c1-c2
 else:
     diafora=c2-c1
-print("Enter dates:",*d1,sep='/',end=' ')
+print("Enter dates:",*d1,sep='/',end=' ') #elegxos megaluterwn kai ektupwsh
 print(*d2,sep='/')
 print(int(diafora),"days")
