31ab70e73331ecb23f299f36f723321c965776dc
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2016-09-30 03:15:56 +0300

    Update exe3d3.py

--- day3 exercises/exe3d3.py
+++ day3 exercises/exe3d3.py
@@ -1,17 +1,17 @@
 num=input('PLEASE ENTER A 10DIGIT NUMBER:')
-number=[int(x) for x in num[0:10]]
+number=[int(x) for x in num[0:10]]   #metatropi se pinaka 
 k=[]
 m=[]
 if len(number) != 10:
-    print("WE SAY 10DIGIT")
+    print("WE SAY 10DIGIT")   #proairetiko
 else:
-    for i in range(0,10):
+    for i in range(0,10):   #2 pinakes gia monos zugous
         if i%2==0:
             k.append(number[i])
         else:
             m.append(number[i])
-            
-print(*k)
+                 
+print(*k)                #print twn pinakwn
 print('',*m)
 
         
