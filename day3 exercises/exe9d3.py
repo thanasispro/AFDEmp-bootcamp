85e297ee35af889bdcc566dc65996c4a08d66bec
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2016-10-12 11:22:09 +0300

    Update exe9d3.py

--- day3 exercises/exe9d3.py
+++ day3 exercises/exe9d3.py
@@ -1,13 +1,15 @@
 word=input('ENTER A WORD:')
-number=int(input('ENTER THE NUMBER OF SHIFT POSITIONS:')) #metatropi grammatwn se ascii
+number=int(input('ENTER THE NUMBER OF SHIFT POSITIONS:'))
 s=list(word)
 k=len(s)
 w=[]
 new=[]
 m=[]
+if number>26:
+    number=number % 26
 for i in s:
-    w.append(ord(i))      #metatropis e aritmous
-for i in range(0,k):        #elegxos oti o arithmos tha kineitaistin perioxi 65-90  grammatwn 
+    w.append(ord(i))
+for i in range(0,k):
     if w[i]+number<=90:
       new.append(w[i]+number)
       m.append(chr(new[i]))
@@ -15,7 +17,7 @@ for i in range(0,k):        #elegxos oti o arithmos tha kineitaistin perioxi 65-
       k=(w[i]+number)-90
       final=64+k
       new.append(final)
-      m.append(chr(new[i]))  #metatropi twn arithmwn se grammata
+      m.append(chr(new[i]))
 print("Enter shift:",number)
 print("Enter phrase:",word)
 print(*m,sep='')
