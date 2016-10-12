ad73ae95b3a6c4811d40476edac645bb83b7d3cc
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2016-10-12 10:45:53 +0300

    Update Pyex1.java

--- Java 1st lab/Pyex1.java
+++ Java 1st lab/Pyex1.java
@@ -3,13 +3,13 @@ public class Pyex1{
 	public static void main(String args[]){
 	int n=9;
     for (int i=1; i<=n; i++) {
-	       int k= 9-i;
-	       do{
+	       
+	       for(int k= 9-i; k>=1; k--){
 			   
-			   k=k-1;
+			   
 			   System.out.print("0");
 			
-		   }while (k>=1);
+		   }
 
     System.out.println(new String(new char[i]).replace("\0", i+""));
     } 
