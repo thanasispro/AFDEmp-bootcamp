83fcc9f50e5626ef6c487a020ee21f34bb731b9a
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2016-10-13 10:19:11 +0300

    Update Dice.java

--- Java 2nd lab/Dice.java
+++ Java 2nd lab/Dice.java
@@ -7,12 +7,12 @@ public class Dice{
 	
      }
     public static void rolling(){
-			int sum1=1;
-			int x1=0;
-		    int x2=0;
+	               int sum1=0;
+		       int x1=0;
+		       int x2=0;
 		    
 		    while(x1!=1 || x2!=1){
-			   x1 = (int)(Math.random()*6) + 1;
+		       x1 = (int)(Math.random()*6) + 1;
 		       x2 = (int)(Math.random()*6) + 1;
 		       sum1++;
 		       }
