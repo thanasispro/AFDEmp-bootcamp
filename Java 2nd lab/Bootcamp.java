6041f3b9b47cb434dcc060379bbeeba856ab3955
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2016-10-13 11:47:44 +0300

    Update Bootcamp.java

--- Java 2nd lab/Bootcamp.java
+++ Java 2nd lab/Bootcamp.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 
 public class Bootcamp {
-			public static void main(String[] args) {
-			System.out.print("Enter the name of pronic numbers:");
-            Scanner n1 = new Scanner(System.in);
-            int number1 = n1.nextInt();
-            System.out.print("Enter the name of pronic numbers:");
-            Scanner n2 = new Scanner(System.in);
-            int number2 = n2.nextInt();
-            ele(number1,number2);
+	public static void main(String[] args) {
+	System.out.print("Enter first number:");
+        Scanner n1 = new Scanner(System.in);
+        int number1 = n1.nextInt();
+        System.out.print("Enter second number:");
+        Scanner n2 = new Scanner(System.in);
+        int number2 = n2.nextInt();
+        ele(number1,number2); /* calling method */
             
 		
 	   }
@@ -18,14 +18,14 @@ public class Bootcamp {
 				 int c=x-y;
 				 int  b= (int)Math.abs(c);
 				 int  m = (int) Math.sqrt(b);
-				 System.out.print(m);
+				 System.out.println("Result: "+m);
                 
 				 
 			 }
 			 else{
 				 
 				 int b=(x+y);
-				 System.out.print(b);
+				 System.out.println("Result: "+b);
 
 				 
 			 }
