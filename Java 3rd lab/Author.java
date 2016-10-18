8d247bdd23a4e237a9e4462d107928c8e53ea524
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2016-10-18 16:09:05 +0300

    Create Author.java

---
+++ Java 3rd lab/Author.java
@@ -0,0 +1,20 @@
+class Author{
+	private String name;
+	
+	Author(){}
+	
+	 Author(String name){
+		 this.name=name;
+	  }
+	
+	public void setName(String name){
+		this.name=name;
+		}
+	 public String getName() { 
+			  return this.name;
+	    }
+	 public String toString(){
+		 return name;
+	    }
+	
+	}
