a69d0ac7e1f2add06059cd5bc82100031586b1d0
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2016-10-21 12:06:43 +0300

    Update TestIn.java

--- Java 5th & 6th  lab/INHERITANCE/src/TestIn.java
+++ Java 5th & 6th  lab/INHERITANCE/src/TestIn.java
@@ -1,21 +1,10 @@
-
 public class TestIn {
 	public static void main(String[] args) {
-		Health cus1=new Health("KONSTANTINA","FIASKOU","WOMAN",1976,58,9);
+		Health cus1=new Health("KONSTANTINA","FIASKOU","FEMALE",1976,58,9);
 		
-		Insurance cus2=new Insurance("NIKOS","GIANAKIS","MAN",1987,43,5);
-		
-		
-		
-				
-		
-		
+		Health cus2=new Health("NIKOS","GIANAKIS","MALE",1976,43,5);
 		cus1.printHealth();
-		cus2.printIn();
-		
-		
-		
-		
-	}
+		cus2.printHealth();
+		}
 
 }
